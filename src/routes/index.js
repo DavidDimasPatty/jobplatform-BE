0235f991385694fea72b112164cc73f423c7eb45
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-07-20 20:23:01 +0700

    add vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { hello,getAllUser,login,signup,getUserFilter,getUserDetail } = require('../controllers/userController');
 
-//router.get('/', hello);
+router.get('/', hello);
 router.get('/getAllUsers', getAllUser);
 router.get('/getUserFilter', getUserFilter);
 router.get('/getUserDetail', getUserDetail);
